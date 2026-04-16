b469958935a5ae139efdec97a0e44ba897d8ae74
gehdron <zenggordon9@gmail.com>
2026-04-16 19:19:27 -0400

    Update CollectionPanel.tsx

--- app/components/CollectionPanel.tsx
+++ app/components/CollectionPanel.tsx
@@ -7,3 +7,46 @@ type Collections = {
     reward_id: number;
     quantity: number;
 }
+
+export default function CollectionsPanel(){
+    const [rewards, setRewards] = useState<Collections[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [message, setmessage] = useState("");
+
+    const loadRewards = async() => {
+        const supabase = createClient();
+
+        const{
+            data:{user},
+            error:userError,
+        } = await supabase.auth.getUser();
+
+        if (userError || !user){
+            setmessage("Log in to view collection");
+            setRewards([]);
+            setLoading(false);
+            return;
+        }
+
+        const {data, error} = await supabase
+            .from("user_rewards")
+            .select("*")
+            .eq("user_id", user.id)
+            .order("quantity", {ascending: false});
+        
+        if(error){
+            setmessage(`Error loading collections : ${error.message}`);
+            setRewards([]);
+            setLoading(false);
+            return;
+        }
+
+        setRewards(data || []);
+        setmessage("");
+        setLoading(false);
+    };
+
+    useEffect(() => {loadRewards();},[])
+
+    
+}
